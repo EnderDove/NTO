de3dce05785a9c0b06b4fa78883cb9dfee873be0
enderdove <golubinskiivladimir@gmail.com>
2022-12-08 21:01:25 +0300

    Added Sword, changes in Stamina and Healt Bar

--- Assets/Scripts/Curse.cs
+++ Assets/Scripts/Curse.cs
@@ -2,17 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Curse : MonoBehaviour
+namespace EnderDove
 {
-    // Start is called before the first frame update
-    void Start()
+    public class Curse : MonoBehaviour
     {
-        
-    }
+        public void OnTriggerEnter(Collider other)
+        {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        }
     }
 }
