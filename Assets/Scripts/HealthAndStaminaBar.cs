4a59e0e84f6b78ca7c86fda0708e0833fb198462
enderdove <golubinskiivladimir@gmail.com>
2022-12-08 03:58:46 +0300

    Continue work under UI

---
+++ Assets/Scripts/HealthAndStaminaBar.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace EnderDove
+{
+    public class HealthAndStaminaBar : MonoBehaviour
+    {
+        public void SetCurentHealt(int healthValue)
+        {
+
+        }
+    }
+}
